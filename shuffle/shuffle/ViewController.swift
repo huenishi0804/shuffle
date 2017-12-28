61bd351425cb54b232d7438b7753a813672f4b4b
hidekazuuenishi <h.uenishi0804@gmail.com>
2017-12-28 14:17:34 +0900

    初めの第一歩

---
+++ shuffle/shuffle/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  shuffle
+//
+//  Created by hidekazu uenishi on 2017/12/28.
+//  Copyright © 2017年 hidekazu uenishi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
